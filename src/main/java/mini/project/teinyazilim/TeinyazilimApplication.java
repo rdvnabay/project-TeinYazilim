d0e9add8a23b58c30ef9ff697c55235e677fc948
Rıdvan <ridvanabay@gmail.com>
2021-07-02 20:30:12 +0300

    First commit Initial

---
+++ src/main/java/mini/project/teinyazilim/TeinyazilimApplication.java
@@ -0,0 +1,13 @@
+package mini.project.teinyazilim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeinyazilimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeinyazilimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mini/project/teinyazilim/TeinyazilimApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.project.teinyazilim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeinyazilimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
