05ae241d136f3f283cfd60082c90d3ca106d2f77
Rıdvan <ridvanabay@gmail.com>
2021-07-05 18:54:05 +0300

    fixed #3  Add Util Result for return type

---
+++ src/main/java/mini/project/teinyazilim/core/utilities/results/ErrorResult.java
@@ -0,0 +1,12 @@
+package mini.project.teinyazilim.core.utilities.results;
+
+public class ErrorResult extends Result {
+
+    public ErrorResult() {
+        super(false);
+    }
+
+    public ErrorResult(String message) {
+        super(false,message);
+    }
+}
